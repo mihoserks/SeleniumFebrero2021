b07b95a570020daaecb21f6144b321ace2804191
sergioRamones <sergio.ramones@cooltesters.com>
2021-03-24 19:51:16 -0600

    Nombres random y nuevos locators

--- SeleniumFeb2021/src/main/java/selenium/SeleniumTest.java
+++ SeleniumFeb2021/src/main/java/selenium/SeleniumTest.java
@@ -1,6 +1,14 @@
 package selenium;
 
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
@@ -18,7 +26,7 @@ public static Logger log = Logger.getLogger(SeleniumTest.class);
 static LeerProperties prop = new LeerProperties();
 
 	public static void main(String[] args) {
-		//C:\Users\srv44\Feb2021\SeleniumFeb2021 \\chromedriver\\chromedriver.exe
+//		C:\Users\srv44\Feb2021\SeleniumFeb2021 \\chromedriver\\chromedriver.exe
 		String rutaChrome = System.getProperty("user.dir") +"\\chromedriver\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", rutaChrome);
 		//Options para abrir 
@@ -38,9 +46,22 @@ static LeerProperties prop = new LeerProperties();
 		driver.get(url);
 		WebElement txt_usernameById = driver.findElement(By.id("txtUsername"));
 		WebElement txt_usernamebyName = driver.findElement(By.name("txtUsername"));
-		WebElement txt_userNameByXpath = driver.findElement(By.xpath("//div[@id='divUsername']/input"));
-//		driver.findElement(By.cssSelector(""));
-//		driver.findElement(By.tagName(""));
+		
+		
+		
+		
+		WebElement txt_userNameCSS = driver.findElement(By.cssSelector("input[name='txtUsername']"));
+		WebElement txt_userNameByXpath = driver.findElement(By.xpath("//input[@name='txtUsername']"));
+		
+		
+		WebElement txt_passwordCSS = driver.findElement(By.cssSelector("input#txtUsername"));
+		
+		
+		WebElement link_forgotpasswordLinkText = driver.findElement(By.linkText("Forgot your password?"));
+		
+		
+//		driver.findElement(By.cssSelector(""));    
+		
 		txt_userNameByXpath.sendKeys("Admin");
 		WebElement txt_password = driver.findElement(By.id("txtPassword"));
 		txt_password.sendKeys("admin123");
@@ -60,16 +81,90 @@ static LeerProperties prop = new LeerProperties();
 			System.out.println("El Login No fue exitoso");
 		}
 		
-		
-		
-		
-		
-	}//main
 	
-	public String randomName() {
-		String[] gente = new String[] {"Sergio","Ivan"};
+		System.out.println(randomName());
+		System.out.println(randomName2());
+		System.out.println(getRandomNumber(10));
+	
 		
-		return "";
+	}//main  
+	
+	public static String randomName() {
+		String[] people = new String[] { "Sergio", "Ivan", "John", "Marcus", "Henry", "Ismael", "Nishant", "Rakesh",
+				"Carlos", "Felix", "Miriam", "Diana", "Adriana", "Alejandro", "Gaby", "Caro", "Melisa", "Aimee",
+				"Nataly", "Fernando", "Thomas", "Fidel", "Javier", "Ricardo", "Monica", "Nidia", "Eddy", "Evert", "Ben",
+				"Anu", "Shan", "Prathap","Rosa","Azucena" };
+		
+		List<String> names = Arrays.asList(people);
+		
+		Collections.shuffle(names);
+		
+		int index = new Random().nextInt(names.size());
+		String randomName = names.get(index);
+		
+		return randomName;
+	}
+	
+	public static String randomName2() {
+		Date date = new Date();
+		
+		List<String> names = new ArrayList<>();
+		names.add("Sergio");
+		names.add("Ivan");
+		names.add("John");
+		names.add("Marcus");
+		names.add("Henry");
+		names.add("Ismael");
+		names.add("Nishant");
+		names.add("Rakesh");
+		names.add("Carlos");
+		names.add("Felix");
+		names.add("Miriam");
+		names.add("Diana");
+		names.add("Adriana");
+		names.add("Alejandro");
+		names.add("Gaby");
+		names.add("Caro");
+		names.add("Melisa");
+		names.add("Aimee");
+		names.add("Nataly");
+		names.add("Fernando");
+		names.add("Thomas");
+		names.add("Fidel");
+		names.add("Javier");
+		names.add("Ricardo");
+		names.add("Monica");
+		names.add("Nidia");
+		names.add("Eddy");
+		names.add("Evert");
+		names.add("Ben");
+		names.add("Anu");
+		names.add("Shan");
+		names.add("Prathap");
+		names.add("Rosa");
+		names.add("Azucena");
+		Collections.shuffle(names);
+		
+		int index = new Random().nextInt(names.size());
+		DateFormat formatt = new SimpleDateFormat("HHmmssddMMyyyy");
+		
+		String randomName = names.get(index);
+
+		return randomName+formatt.format(date)+"@gmail.com";
+	}
+	
+	
+	public static String getRandomNumber(double length) {
+		Random random = new Random();
+		int randomNumber = 0;
+		Boolean flag = true;
+		while (flag) {
+			randomNumber = random.nextInt();
+			if (Double.toString(randomNumber).length() == length && !Double.toString(randomNumber).startsWith("-")) {
+				flag = false;
+			}
+		}
+		return String.valueOf(randomNumber);
 	}
 
 }
