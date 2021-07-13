197dbfe7b533b39427ae441366eb27c426b6a098
mihoserks <mihoser_@hotmail.com>
2021-07-13 04:34:38 -0500

    PrimeScript

--- SeleniumFeb2021/src/test/java/jesusFlores/PrimerScriptSelenium.java
+++ SeleniumFeb2021/src/test/java/jesusFlores/PrimerScriptSelenium.java
@@ -1,5 +1,21 @@
 package jesusFlores;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class PrimerScriptSelenium {
+	
+	public static WebDriver driver;
+	public static void main(String[] args) {
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--start-maximized");
+		option.addArguments("--incognito");
+		System.setProperty("webdriver.chrome.driver", "chromedriver\\chromedriver.exe");
+	
+	driver = new ChromeDriver(option);
+	driver.get("https://opensource-demo.orangehrmlive.com/");
+	//driver.close();
+	}
 
 }
